d329779fe55139a0a157017b4db97fbfc8a949dc
AXON <axonasif@gmail.com>
2022-09-18 20:52:17 +0600

    Improvements

--- src/main.sh
+++ src/main.sh
@@ -1,11 +1,12 @@
 use std::print::log;
+use std::native::sleep;
 
 function stream_gp_ports() {
 	export cppid=$PPID;
 	echo "$cppid" > /tmp/ppid
   function read_ports() {
 	  while sleep 2 && grep -q '@pts/' "/proc/$cppid/cmdline"; do {
-		lsof -P -iTCP -sTCP:LISTEN -F 'n' | grep 'n.*:' | sed 's|.*:||g'	
+		lsof -P -iTCP -sTCP:LISTEN -F 'n' | sed -n 's/^n\*://p';
 	} done
   }
 
@@ -17,7 +18,10 @@ function stream_gp_ports() {
           if [[ ! "${forwarded_ports[*]}" =~ (^| )${port}($| ) ]]; then {
                   forwarded_ports+=("$port");
 		  if test -v tmux; then {
-			tmux display-message -t main "Port ${port} was forwarded to your client device";
+			  while read -r session_name; do {
+				tmux display-message -t "$session_name" "Port ${port} was forwarded to your client device";
+			  } done < <(tmux list-sessions -F "#{session_name}" 2>/dev/null);
+		  	unset session_name;
 		  } fi
                   printf '%s\n' "$port";
           } fi
@@ -28,7 +32,7 @@ function main() {
   local args=("${@}");
   # ran_port=$(($RANDOM * $RANDOM * $RANDOM)) && ran_port="${ran_port::4}";
   # address="localhost:$ran_port";
-  local master_socket="/tmp/.gssh_$RANDOM";
+  local master_socket="/tmp/.gssh_sock";
 
   for i in "${!args[@]}"; do {
 	if [[ "${args[i]}" == "--" ]]; then {
@@ -51,6 +55,7 @@ function main() {
 	    ssh
 		    #-C # For compression
 		    -M -S "$master_socket"
+		    -o ControlPersist=10m
 		    -o UserKnownHostsFile=/dev/null
 		    -o StrictHostKeyChecking=no
 		    "$server_address"
@@ -66,9 +71,10 @@ function main() {
     host="127.0.0.1";
     while read -r port; do {
       # Avoid conflict for multiple ssh connections
-      if nc -w 1 -z "$host" "$port" 2>/dev/null; then {
-        continue;
-      } fi
+      while nc -w 1 -z "$host" "$port" 2>/dev/null; do {
+	      log::warn "Port $port already exists, adding 1 to $port to try again";
+	      port="$((port + 1))"
+      } done
       ssh -n -O forward -S "$master_socket" -L "${port}:${host}:${port}" "$server_address";
     } done < <(
       printf '%s\n' "$(declare -f stream_gp_ports)" "stream_gp_ports;exit" \
